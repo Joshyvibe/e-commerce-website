38264edbaa14db2e0e03eaf36aaa3f983c93dbed
Obinna Akpulonu <akpulonujoseph@gmail.com>
2023-01-23 13:54:14 +0100

    Update settings.py

--- online_store/settings.py
+++ online_store/settings.py
@@ -151,6 +151,6 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 465
-EMAIL_HOST_USER = os.environ.get('akpulonuchioma@gmail.com')
-EMAIL_HOST_PASSWORD = os.environ.get('cfjbmljzgqgyszmz')
+EMAIL_HOST_USER = os.environ.get('your_email@gmail.com')
+EMAIL_HOST_PASSWORD = os.environ.get('your_email_password')
 
